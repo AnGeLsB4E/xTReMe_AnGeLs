b791a0335f7ae71c2d54d0ab91a1286e63f662f1
AnGeLsB4E <xtreme.angels06@gmail.com>
2013-07-02 13:36:22 +0200

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -558,7 +558,7 @@ function populateUserlist()
 
         $('#plugbot-userlist').append('<h1 style="text-indent:12px;color:#42A5DC;font-size:14px;font-variant:small-caps;">Utilisateurs: ' + API.getUsers().length + '</h1>');
 
-        $('#plugbot-userlist').append('<p style="padding-left:12px;text-indent:0px !important;font-style:italic;color:#FFFF6B;font-size:12px;">Rejoin notre site<br />boost4ever.fr</p><br />');
+        $('#plugbot-userlist').append('<p style="padding-left:12px;text-indent:0px !important;font-style:italic;color:#FFFF6B;font-size:12px;">Rejoins notre site sur<br />boost4ever.fr</p><br />');
 
         if ($('#button-dj-waitlist-view').attr('title') !== '')
         {
@@ -566,7 +566,7 @@ function populateUserlist()
                         ($.inArray(API.getDJs(), API.getSelf()) == -1)) {
                         var spot = $('#button-dj-waitlist-view').attr('title').split('(')[1];
                         spot = spot.substring(0, spot.indexOf(')'));
-                        $('#plugbot-userlist').append('<h1 id="plugbot-queuespot"><span style="font-variant:small-caps">Waitlist:</span> ' + spot + '</h1><br />');
+                        $('#plugbot-userlist').append('<h1 id="plugbot-queuespot"><span style="font-variant:small-caps">Liste D'attente:</span> ' + spot + '</h1><br />');
                 }
         }
 
